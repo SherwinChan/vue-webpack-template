df298549dc9eb3a8f38877d8b7f11963b8ffecaf
SherwinChan <409863416@qq.com>
2017-09-04 14:44:18 +0800

    修改默认api地址

--- template/src/libs/util.js
+++ template/src/libs/util.js
@@ -6,19 +6,19 @@ let util = {
 
 };
 
-let ajaxUrl = 'http://yimentong.dev.sj33333.com/index.php'; //默认的api地址
+let ajaxUrl = 'http://blog.dev.sherwin.com/index.php'; //默认的api地址
 
 {{#vuex}}store.commit('setEnv', env);{{/vuex}}
 if (env === 'development') {
   {{#vuex}}
   let server = localStorage.getItem('server');  //server是api的域名地址，如master、dev等
   if (server) {
-    let url = 'http://yimentong.' + server + '.sj33333.com/index.php';  //切换的api地址
+    let url = 'http://blog.' + server + '.sherwin.com/index.php';  //切换的api地址
     store.commit('setServer', server) //记录server进vuex
     ajaxUrl = url;
   } else {
     // 若无server选项，则本地开发填默认api地址，线上则默认服务器域名地址
-    {{/vuex}}ajaxUrl = window.location.hostname == 'localhost' ? 'http://yimentong.master.sj33333.com/index.php' : window.location.origin + '/index.php';
+    {{/vuex}}ajaxUrl = window.location.hostname == 'localhost' ? 'http://blog.sherwin.com/index.php' : window.location.origin + '/index.php';
     {{#vuex}}
   }{{/vuex}}
 } else {
