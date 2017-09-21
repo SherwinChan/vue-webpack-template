cd5797a20efc5c41d22a43265c2133a6778774fe
SherwinChan <409863416@qq.com>
2017-09-21 12:03:32 +0800

    babel 新增useBuiltIns选项

---
+++ template/src/components/menu.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div>
+    menu
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menu',
+  data(){
+    return {
+
+    }
+  },
+  created(){
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
+
--- template/src/libs/util.js
+++ template/src/libs/util.js
@@ -10,17 +10,7 @@ let ajaxUrl = 'http://blog.dev.sherwin.com/index.php'; //默认的api地址
 
 {{#vuex}}store.commit('setEnv', env);{{/vuex}}
 if (env === 'development') {
-  {{#vuex}}
-  let server = localStorage.getItem('server');  //server是api的域名地址，如master、dev等
-  if (server) {
-    let url = 'http://blog.' + server + '.sherwin.com/index.php';  //切换的api地址
-    store.commit('setServer', server) //记录server进vuex
-    ajaxUrl = url;
-  } else {
-    // 若无server选项，则本地开发填默认api地址，线上则默认服务器域名地址
-    {{/vuex}}ajaxUrl = window.location.hostname == 'localhost' ? 'http://blog.sherwin.com/index.php' : window.location.origin + '/index.php';
-    {{#vuex}}
-  }{{/vuex}}
+  ajaxUrl = window.location.hostname == 'localhost' ? 'http://blog.dev.sherwin.com/index.php' : window.location.origin + '/index.php';
 } else {
   if (env === 'production') {
     // 生产环境的api以服务器域名地址为准
