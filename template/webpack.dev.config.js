c21b3d63c7849ffa03edc9a2e3f42192c015e529
SherwinChan <409863416@qq.com>
2017-09-29 17:17:34 +0800

    新增vconsole移动端调试工具安装选项

--- template/webpack.dev.config.js
+++ template/webpack.dev.config.js
@@ -3,7 +3,8 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const webpackBaseConfig = require('./webpack.config.js');
-const fs = require('fs');
+const fs = require('fs');{{#vconsole-webpack-plugin}}
+const vConsolePlugin = require('vconsole-webpack-plugin');{{/vconsole-webpack-plugin}}
 
 fs.open('./src/config/env.js', 'w', function (err, fd) {
   const buf = 'export default "development";';
@@ -16,12 +17,16 @@ module.exports = merge(webpackBaseConfig, {
     publicPath: '/dist/',
   },
   plugins: [
+    // 在项目处生成一个html供localhost访问
     new HtmlWebpackPlugin({
       filename: '../index.html',
       template: path.resolve(__dirname, './src/template/index.html'),
       inject: true, //scripte插入到body底部
       // minify: false,  //压缩 {...} | false
       // hash: true, //是否生成hash值，默认false
-    }),
+    }){{#vconsole-webpack-plugin}},
+    new vConsolePlugin({
+      enable: true // 开启vconsole调试工具，发布代码前记得改回 false
+    }),{{/vconsole-webpack-plugin}}
   ]
 });
