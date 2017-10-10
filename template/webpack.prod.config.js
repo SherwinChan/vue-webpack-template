0f886afbbc845d44dd256cc5de7d125ad0d0b7cd
SherwinChan <409863416@qq.com>
2017-10-10 11:22:58 +0800

    webpack.pro.config.js修改

--- template/webpack.prod.config.js
+++ template/webpack.prod.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const webpackBaseConfig = require('./webpack.config.js');
 const fs = require('fs');
