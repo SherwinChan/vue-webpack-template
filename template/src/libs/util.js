4ebf0eaad82879a61b538ee27d3c2ab220d4a041
SherwinChan <409863416@qq.com>
2018-02-05 16:00:03 +0800

    优化util.js

--- template/src/libs/util.js
+++ template/src/libs/util.js
@@ -10,7 +10,7 @@ let ajaxUrl = 'http://blog.dev.sherwin.com/index.php'; //默认的api地址
 
 {{#vuex}}store.commit('setEnv', env);{{/vuex}}
 if (env === 'development') {
-  ajaxUrl = window.location.hostname == 'localhost' ? 'http://blog.dev.sherwin.com/index.php' : window.location.origin + '/index.php';
+  ajaxUrl = window.location.hostname == 'localhost' ? ajaxUrl : window.location.origin + '/index.php';
 } else {
   if (env === 'production') {
     // 生产环境的api以服务器域名地址为准
