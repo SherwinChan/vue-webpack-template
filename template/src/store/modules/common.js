2adf260f19346026354a54c520070ec06513d3ac
sherwin <409863416@qq.com>
2019-05-08 14:26:40 +0800

    release

--- template/src/store/modules/common.js
+++ template/src/store/modules/common.js
@@ -1,7 +1,19 @@
 const state = {
   listRows: 15,
+  env: 'production', //运行环境  development || production
+  baseURL: '', //axios的api域名头，http://ymtpartybuild.dev.sj33333.com/app
+}
+
+const mutations = {
+  setEnv(state, environment) {
+    state.env = environment;
+  },
+  setBaseURL(state, baseURL) {
+    state.baseURL = baseURL;
+  },
 }
 
 export default {
-  state
+  state,
+  mutations
 }
